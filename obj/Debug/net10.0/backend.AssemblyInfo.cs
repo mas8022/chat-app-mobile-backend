526a4982632ff4f87a29f9c3c8a2b8a78d7a36c1
masproo22 <m.abbaszadeh2002@gmail.com>
2026-09-07 22:32:47 +0330

    feat: start project

--- obj/Debug/net10.0/backend.AssemblyInfo.cs
+++ obj/Debug/net10.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c2ef417bec449c9ce18979ad46f9cccca0b6de1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad25cf08bd2f753c3b57309a5241e27edab17a79")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
